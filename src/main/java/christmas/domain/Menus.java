9697fd15f64f97289bd8ad45f4022159a4909b86
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-10 16:32:55 +0900

    feat: 메뉴 항목 당 주문량을 나타내는 변수 추가

--- src/main/java/christmas/domain/Menus.java
+++ src/main/java/christmas/domain/Menus.java
@@ -4,27 +4,29 @@ import java.util.Arrays;
 
 public enum Menus {
 
-    SOUP("양송이수프", 6000, "애피타이저"),
-    TAPAS("타파스", 5000, "애피타이저"),
-    SALAD("시저샐러드", 8000, "애피타이저"),
-    TBONE("티본스테이크",55000, "메인"),
-    RIB("바비큐립", 54000, "메인"),
-    SEAPASTA("해산물파스타", 35000, "메인"),
-    CHRISTMASPASTA("크리스마스파스타", 25000, "메인"),
-    CAKE("초코케이크", 15000, "디저트"),
-    ICECREAM("아이스크림", 5000, "디저트"),
-    ZEROCOKE("제로콜라", 3000, "음료"),
-    REDWINE("레드와인", 60000, "음료"),
-    CHAMPAGNE("샴페인", 25000, "음료");
+    SOUP("양송이수프", 6000, "애피타이저", 0),
+    TAPAS("타파스", 5000, "애피타이저", 0),
+    SALAD("시저샐러드", 8000, "애피타이저", 0),
+    TBONE("티본스테이크",55000, "메인", 0),
+    RIB("바비큐립", 54000, "메인", 0),
+    SEAPASTA("해산물파스타", 35000, "메인", 0),
+    CHRISTMASPASTA("크리스마스파스타", 25000, "메인", 0),
+    CAKE("초코케이크", 15000, "디저트", 0),
+    ICECREAM("아이스크림", 5000, "디저트", 0),
+    ZEROCOKE("제로콜라", 3000, "음료", 0),
+    REDWINE("레드와인", 60000, "음료", 0),
+    CHAMPAGNE("샴페인", 25000, "음료", 0);
 
     private final String menuName;
     private final int cost;
     private final String category;
+    private int count;
 
-    Menus (String menuName, int cost, String category) {
+    Menus (String menuName, int cost, String category, int count ) {
         this.menuName = menuName;
         this.cost = cost;
         this.category = category;
+        this.count = count;
     }
     public int getCost() {
         return cost;
@@ -32,11 +34,25 @@ public enum Menus {
     public String getCategory() {
         return category;
     }
+    public int getCount() {
+        return count;
+    }
     public static String getCateogoryByNames(String menuName) {
         return Arrays.stream(Menus.values())
                 .filter(menu -> menu.menuName.equals(menuName))
                 .findFirst()
                 .map(menu -> menu.category)
+                .orElse(null);
+    }
+
+    public static Menus updateCount(String menuName, int updateCount) {
+        return Arrays.stream(Menus.values())
+                .filter(menu -> menu.menuName.equals(menuName))
+                .findFirst()
+                .map(menu -> {
+                    menu.count = updateCount;
+                    return menu;
+                })
                 .orElse(null);
     }
 }
