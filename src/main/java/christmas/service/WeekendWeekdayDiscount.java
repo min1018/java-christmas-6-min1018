44ed453a5b30a7346d03cc6bb1b416ec07b871fc
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-11 16:11:31 +0900

    feat: 평일, 주말 여부에 따른 할인 이벤트 적용 기능 추가

---
+++ src/main/java/christmas/service/WeekendWeekdayDiscount.java
@@ -0,0 +1,39 @@
+package christmas.service;
+
+import christmas.domain.Menus;
+import christmas.domain.User;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WeekendWeekdayDiscount {
+    private static final List<Integer> mainMenuDiscount = new ArrayList<>(Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30));
+    public static void whichDiscountAvailable(int date, User user) {
+        List<Menus> menus = user.getOrderedMenus;
+        if (mainMenuDiscount.contains(date)) {
+            weekendsDiscount(menus);
+        }
+        weekdaysDiscount(menus);
+    }
+
+    private static int weekdaysDiscount(List<Menus> menus) {
+        int totalDiscount = 0;
+        for(Menus menu : menus) {
+            if (menu.getCategory() == "디저트") {
+                totalDiscount += 2023;
+            }
+        }
+        return totalDiscount;
+    }
+
+    private static int weekendsDiscount(List<Menus> menus) {
+        int totalDiscount = 0;
+        for (Menus menu: menus) {
+            if (menu.getCategory() == "디저트") {
+                totalDiscount += 2023;
+            }
+        }
+        return totalDiscount;
+    }
+}
