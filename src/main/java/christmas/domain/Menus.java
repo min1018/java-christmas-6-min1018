bf54d037f6e0326c0968d3b3f7edd4e05d204aad
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-12 19:13:27 +0900

    fix: hash 사용해서 수량 처리

--- src/main/java/christmas/domain/Menus.java
+++ src/main/java/christmas/domain/Menus.java
@@ -1,32 +1,38 @@
 package christmas.domain;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Arrays;
 
 public enum Menus {
 
-    SOUP("양송이수프", 6000, "애피타이저", 0),
-    TAPAS("타파스", 5000, "애피타이저", 0),
-    SALAD("시저샐러드", 8000, "애피타이저", 0),
-    TBONE("티본스테이크",55000, "메인", 0),
-    RIB("바비큐립", 54000, "메인", 0),
-    SEAPASTA("해산물파스타", 35000, "메인", 0),
-    CHRISTMASPASTA("크리스마스파스타", 25000, "메인", 0),
-    CAKE("초코케이크", 15000, "디저트", 0),
-    ICECREAM("아이스크림", 5000, "디저트", 0),
-    ZEROCOKE("제로콜라", 3000, "음료", 0),
-    REDWINE("레드와인", 60000, "음료", 0),
-    CHAMPAGNE("샴페인", 25000, "음료", 0);
+    양송이수프("양송이수프", 6000, "애피타이저"),
+    타파스("타파스", 5000, "애피타이저"),
+    시저샐러드("시저샐러드", 8000, "애피타이저"),
+    티본스테이크("티본스테이크",55000, "메인"),
+    바베큐립("바비큐립", 54000, "메인"),
+    해산물파스타("해산물파스타", 35000, "메인"),
+    크리스마스파스타("크리스마스파스타", 25000, "메인"),
+    초코케이크("초코케이크", 15000, "디저트"),
+    아이스크림("아이스크림", 5000, "디저트"),
+    제로콜라("제로콜라", 3000, "음료"),
+    레드와인("레드와인", 60000, "음료"),
+    샴페인("샴페인", 25000, "음료");
 
     private final String menuName;
     private final int cost;
     private final String category;
     private int count;
 
-    Menus (String menuName, int cost, String category, int count ) {
+    private static final Map<String, Integer> menuCounts = new HashMap<>();
+
+    Menus (String menuName, int cost, String category ) {
         this.menuName = menuName;
         this.cost = cost;
         this.category = category;
-        this.count = count;
+    }
+    public String getMenuName() {
+        return menuName;
     }
     public int getCost() {
         return cost;
@@ -35,7 +41,11 @@ public enum Menus {
         return category;
     }
     public int getCount() {
-        return count;
+        return menuCounts.getOrDefault(menuName, 0);
+    }
+
+    public void updateCount(int updateCount) {
+        menuCounts.put(menuName, updateCount);
     }
     public static String getCateogoryByNames(String menuName) {
         return Arrays.stream(Menus.values())
@@ -45,14 +55,4 @@ public enum Menus {
                 .orElse(null);
     }
 
-    public static Menus updateCount(String menuName, int updateCount) {
-        return Arrays.stream(Menus.values())
-                .filter(menu -> menu.menuName.equals(menuName))
-                .findFirst()
-                .map(menu -> {
-                    menu.count = updateCount;
-                    return menu;
-                })
-                .orElse(null);
-    }
 }
