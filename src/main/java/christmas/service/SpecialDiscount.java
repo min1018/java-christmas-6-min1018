486d0516b4d81d75daa138dd2c47147752281410
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-12 16:20:56 +0900

    fix: 파라미터 순서 수정

--- src/main/java/christmas/service/SpecialDiscount.java
+++ src/main/java/christmas/service/SpecialDiscount.java
@@ -10,7 +10,7 @@ import christmas.domain.User;
 public class SpecialDiscount {
     private static final List<Integer> specialDayDiscount = new ArrayList<>(Arrays.asList(3, 10, 17, 24, 31, 25));
 
-    public static DiscountHistory isAvailable(DiscountHistory discountHistory, User user) {
+    public static DiscountHistory isAvailable(User user, DiscountHistory discountHistory) {
         if (specialDayDiscount.contains(user.getDate())) {
             discountHistory.updateSpecial(1000);
             return discountHistory;
