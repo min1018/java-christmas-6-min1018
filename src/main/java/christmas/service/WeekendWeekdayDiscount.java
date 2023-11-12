55940c96c1640aac56b0c5a2dbf11244699128e6
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-12 23:30:37 +0900

    fix: 수량만큼 할인 기능 수정

--- src/main/java/christmas/service/WeekendWeekdayDiscount.java
+++ src/main/java/christmas/service/WeekendWeekdayDiscount.java
@@ -1,6 +1,6 @@
 package christmas.service;
 
-import christmas.domain.DiscountHistory;
+import christmas.domain.DiscountOption;
 import christmas.domain.Menus;
 import christmas.domain.User;
 
@@ -10,34 +10,34 @@ import java.util.List;
 
 public class WeekendWeekdayDiscount {
     private static final List<Integer> mainMenuDiscount = new ArrayList<>(Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30));
-    public static DiscountHistory whichDiscountAvailable(User user, DiscountHistory discountHistory) {
+    public static User whichDiscountAvailable(User user) {
         List<Menus> menus = user.getOrderedMenus();
 
         if (mainMenuDiscount.contains(user.getDate())) {
-            return weekendsDiscount(discountHistory, menus);
+            return weekendsDiscount(user, menus);
         }
-        return weekdaysDiscount(discountHistory,menus);
+        return weekdaysDiscount(user,menus);
     }
 
-    private static DiscountHistory weekdaysDiscount(DiscountHistory discountHistory,List<Menus> menus) {
+    private static User weekdaysDiscount(User user, List<Menus> menus) {
         int totalDiscount = 0;
         for(Menus menu : menus) {
             if (menu.getCategory() == "디저트") {
-                totalDiscount += 2023;
+                totalDiscount += 2023 * menu.getCount();
             }
         }
-        discountHistory.updateWeekdays(totalDiscount);
-        return discountHistory;
+        user.moreDiscount(totalDiscount, "평일");
+        return user;
     }
 
-    private static DiscountHistory weekendsDiscount(DiscountHistory discountHistory, List<Menus> menus) {
+    private static User weekendsDiscount(User user, List<Menus> menus) {
         int totalDiscount = 0;
         for (Menus menu: menus) {
             if (menu.getCategory() == "메인") {
-                totalDiscount += 2023;
+                totalDiscount += 2023 * menu.getCount();
             }
         }
-        discountHistory.updateWeekend(totalDiscount);
-        return discountHistory;
+        user.moreDiscount(totalDiscount, "주말");
+        return user;
     }
 }
