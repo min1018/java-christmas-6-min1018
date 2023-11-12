fae770291b689e58c947b20b9a786a65f85859b8
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-12 23:31:48 +0900

    fix:에러 수정

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -2,16 +2,22 @@ package christmas.controller;
 
 import christmas.domain.Menus;
 import christmas.domain.User;
+import christmas.service.BonusDiscount;
+import christmas.service.DdayDiscount;
+import christmas.service.SpecialDiscount;
+import christmas.service.WeekendWeekdayDiscount;
 import christmas.utils.DateException;
 import christmas.utils.MenuException;
 import christmas.view.InputView;
+import christmas.view.OutputView;
 
 import java.util.List;
 
 public class Controller {
     public static void run () {
-        userReservationSetting();
-
+        User user = userReservationSetting();
+        user = orderHistoryBeforeDiscount(user);
+        showDiscountHistory(user);
     }
     private static User userReservationSetting() {
         User user = new User(reservationDateSetting(), reservationMenuSetting());
@@ -42,4 +48,31 @@ public class Controller {
         return menus;
     }
 
+    private static User orderHistoryBeforeDiscount(User user) {
+        OutputView.printOrders(user);
+        OutputView.printPaymentBeforeDiscount(user);
+        if(user.getTotalOrderBeforeDiscount() > 10000) {
+            user = applyingDiscount(user);
+        }
+        return user;
+    }
+    private static User applyingDiscount(User user) {
+        user = ApplyDiscount(user);
+        return user;
+    }
+
+    private static void showDiscountHistory(User user) {
+        OutputView.printBonusDiscount(user);
+        OutputView.printDiscountHistory(user);
+        OutputView.printTotalDiscount(user);
+        OutputView.printPaymentAfterDiscount(user);
+        OutputView.printBadge(user);
+    }
+    private static User ApplyDiscount(User user) {
+       user = DdayDiscount.calcDday(user);
+       user = BonusDiscount.isAvaliable(user);
+       user = SpecialDiscount.isAvailable(user);
+       user = WeekendWeekdayDiscount.whichDiscountAvailable(user);
+       return user;
+    }
 }
