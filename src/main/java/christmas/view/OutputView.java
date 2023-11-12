8db9b16ddc48ea366183a22e1598ebcec156d292
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-12 22:55:17 +0900

    feat: 출력 양식에 맞추는 메소드 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,12 +1,14 @@
 package christmas.view;
 
-import christmas.domain.DiscountHistory;
+import christmas.domain.DiscountOption;
 import christmas.domain.Menus;
 import christmas.domain.User;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class OutputView {
+    public static final String EVENT_START = "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
     public static final String ORDER_MENU = "<주문 메뉴>";
     public static final String BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
     public static final String BONUS_MENU = "<증정 메누>";
@@ -23,6 +25,7 @@ public class OutputView {
     public static final String ANNOUNCE_BADGE = "<12월 이벤트 배지>";
 
     static public void printOrders(User user) {
+        System.out.println("12월 "+user.getDate()+EVENT_START);
         System.out.println(ORDER_MENU);
         List<Menus> menus = user.getOrderedMenus();
         for (Menus menu : menus) {
@@ -36,42 +39,68 @@ public class OutputView {
         System.out.println(user.getTotalOrderBeforeDiscount()+"원"+"\n");
     }
 
-    static public void printBonusDiscount(DiscountHistory discountHistory) {
+    static public void printBonusDiscount(User user) {
         System.out.println(BONUS_MENU);
-        if (discountHistory.getBonusDiscount() == 0) {
+        if (user.getDiscountAmountForOption("증정") == 0) {
             System.out.println("없음");
         }
-        if (discountHistory.getBonusDiscount() != 0) {
+        if (user.getDiscountAmountForOption("증정") != 0) {
             System.out.println(BONUS_CHAMPAGNE+"\n");
         }
     }
-    static public void printDiscountHistory(DiscountHistory history) {
+    static public void printDiscountHistory(User user) {
         System.out.println(DETAIL_DISCOUNT);
-        System.out.println(CHRISTMAS_DDAY + history.getDdayDiscount()+WON);
-        printWeekendOrWeekday(history);
-        System.out.println(SPECIAL_DISCOUNT + history.getSpecialDiscount()+WON);
-        System.out.println(BONUS_DISCOUNT + history.getBonusDiscount()+WON+"\n");
+        if (user.isUserDiscountEmpty()) {
+            System.out.println("없음\n");
+        }
+        if (!user.isUserDiscountEmpty()) {
+            printDdayDiscount(user);
+            printWeekendOrWeekday(user);
+            printSpecialDiscount(user);
+            printBonusDiscountAmount(user);
+            System.out.println();
+        }
 
     }
-    static public void printWeekendOrWeekday(DiscountHistory history) {
-        if(history.getWeekdayDiscount() == 0) {
-            System.out.println(WEEKEND_DISCOUNT + history.getWeekendDiscount()+WON);
+    static public void printDdayDiscount(User user) {
+        if(user.getDiscountAmountForOption("디데이") != 0) {
+            System.out.println(CHRISTMAS_DDAY + formatNumber(user.getDiscountAmountForOption("디데이")) +WON);
         }
-        if(history.getWeekdayDiscount() != 0) {
-            System.out.println(WEEKDAY_DISCOUNT +history.getWeekdayDiscount()+WON);
+    }
+    static public void printWeekendOrWeekday(User user) {
+        if(user.getDiscountAmountForOption("주말") != 0) {
+            System.out.println(WEEKEND_DISCOUNT + formatNumber(user.getDiscountAmountForOption("주말"))+WON);
+        }
+        if(user.getDiscountAmountForOption("평일") != 0) {
+            System.out.println(WEEKDAY_DISCOUNT + formatNumber(user.getDiscountAmountForOption("평일"))+WON);
+        }
+    }
+    static public void printSpecialDiscount(User user) {
+        if(user.getDiscountAmountForOption("특별") != 0) {
+            System.out.println(SPECIAL_DISCOUNT + formatNumber(user.getDiscountAmountForOption("특별"))+WON);
+        }
+    }
+    static public void printBonusDiscountAmount(User user) {
+        if(user.getDiscountAmountForOption("증정") != 0) {
+            System.out.println(BONUS_DISCOUNT + formatNumber(user.getDiscountAmountForOption("증정"))+WON);
         }
     }
 
-    static public void printTotalDiscount(DiscountHistory history) {
+    static public void printTotalDiscount(User user) {
         System.out.println(ANNOUNCE_DISCOUNT);
-        System.out.println("-"+history.getTotalDiscount()+WON+"\n");
+        System.out.println("-"+formatNumber(user.getTotalOrderBeforeDiscount())+WON+"\n");
     }
-    static public void printPaymentAfterDiscount(DiscountHistory history, User user) {
+    static public void printPaymentAfterDiscount(User user) {
         System.out.println(ANNOUNCE_AFTER_DISCOUNT);
-        System.out.println(user.getTotalOrderBeforeDiscount() - history.getTotalDiscount()+WON+"\n");
+        System.out.println(formatNumber(user.getTotalOrderBeforeDiscount() - user.getTotalDiscount()) +WON+"\n");
     }
     static public void printBadge(User user) {
         System.out.println(ANNOUNCE_BADGE);
-        System.out.println(user.getUserBadge());
+        System.out.println(user.whichBadge(user.getTotalDiscount()));
+    }
+
+    static public String formatNumber(long number) {
+        DecimalFormat decimalFormat = new DecimalFormat("#,###");
+        return decimalFormat.format(number);
     }
 }
