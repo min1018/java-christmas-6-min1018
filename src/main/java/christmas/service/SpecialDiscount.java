c964f61f4ef52e09330ed82eb11ddddf2235b98d
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-12 16:05:37 +0900

    refactor: DiscountHistory를 반환하게끔 수정

--- src/main/java/christmas/service/SpecialDiscount.java
+++ src/main/java/christmas/service/SpecialDiscount.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import christmas.domain.DiscountHistory;
+import christmas.domain.User;
+
 public class SpecialDiscount {
     private static final List<Integer> specialDayDiscount = new ArrayList<>(Arrays.asList(3, 10, 17, 24, 31, 25));
 
-    public static boolean isAvailable(int date) {
-        if (specialDayDiscount.contains(date)) {
-            return true;
+    public static DiscountHistory isAvailable(DiscountHistory discountHistory, User user) {
+        if (specialDayDiscount.contains(user.getDate())) {
+            discountHistory.updateSpecial(1000);
+            return discountHistory;
         }
-        return false;
+        return discountHistory;
     }
 
 }
