33c58b5814c477e22a1fe1ceed7983f8578274e5
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-13 14:42:36 +0900

    Test: 5,000 미만의 금액일 경우 '없음' 반환 여부 테스트 추가

--- src/test/java/christmas/domain/BadgeTest.java
+++ src/test/java/christmas/domain/BadgeTest.java
@@ -10,17 +10,22 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class BadgeTest {
     @Test
     @DisplayName("5100원의 혜택을 받았을 시 별 뱃지를 출력해야한다.")
-    void StarBadgeTest() {
+    void starBadgeTest() {
         assertThat(Badge.valueOf(5100)).isEqualTo(Badge.Star);
     }
     @Test
     @DisplayName("1100원의 혜택을 받았을 시 트리 뱃지를 출력해야한다.")
-    void TreeTest() {
+    void treeBadgeTest() {
         assertThat(Badge.valueOf(11000)).isEqualTo(Badge.Tree);
     }
     @Test
     @DisplayName("2100원의 혜택을 받았을 시 산타 뱃지를 출력해야한다.")
-    void SantaTest() {
+    void santaBadgeTest() {
         assertThat(Badge.valueOf(21000)).isEqualTo(Badge.Santa);
+    }
+    @Test
+    @DisplayName("3000원의 혜택을 받았을 시 없음을 출력해야한다.")
+    void noBadgeTest() {
+        assertThat(Badge.valueOf(3000)).isEqualTo(Badge.None);
     }
 }
